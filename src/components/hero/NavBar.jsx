d43d23443c7e4ca41784a66fcc671b6c1f549e4e
lasith herath <lasithherath00@gmail.com>
2024-12-22 21:35:08 +0530

    Add react-icons and implement responsive drawer in NavBar component

--- src/components/hero/NavBar.jsx
+++ src/components/hero/NavBar.jsx
@@ -1,15 +1,38 @@
-import React from 'react';
 import MainLogo from '../../assets/mainLogo1.png';
+import React, { useState } from 'react';
+import { MdClose } from 'react-icons/md';
+import { TiThMenuOutline } from 'react-icons/ti';
+import { Drawer, Button } from '@mui/material';
 const NavBar = () => {
+  const [isDrawerVisible, setIsDrawerVisible] = useState(false);
+  const showDrawer = () => {
+    setIsDrawerVisible(true);
+  };
+
+  const closeDrawer = () => {
+    setIsDrawerVisible(false);
+  };
   return (
     <header className="lg:inline-block lg:relative  bg-[#333333AB] p-4 text-white items-center justify-between lg:rounded-full w-full lg:mt-12 lg:mx-40">
       {/* Logo */}
-      <div className="lg:absolute  lg:top-1/2 lg:transform lg:-translate-y-1/2 lg:-translate-x-20">
+      <div className="flex justify-between  items-center lg:flex-none lg:justify-normal lg:absolute  lg:top-1/2 lg:transform lg:-translate-y-1/2 lg:-translate-x-20">
         <img
           src={MainLogo}
           alt="Starbeans Ceylon"
           className="lg:w-[36%] w-[20%] md:w-[10%] "
         />
+        <div className="lg:hidden  ">
+          <div
+            onClick={isDrawerVisible ? closeDrawer : showDrawer}
+            className="flex lg:hidden mr-5"
+          >
+            {isDrawerVisible ? (
+              <MdClose size={24} color="white" />
+            ) : (
+              <TiThMenuOutline size={24} />
+            )}
+          </div>
+        </div>
       </div>
 
       {/* Navigation Links */}
@@ -45,6 +68,17 @@ const NavBar = () => {
           Contact
         </a>
       </div>
+      <Drawer anchor="left" onClose={closeDrawer} open={isDrawerVisible}>
+        <div className="flex flex-col gap-5 ">
+          <div className="mt-6 flex flex-col gap-10 mx-10 text-[#181F26] text-[20px] font-sans font-semibold">
+            <h1 className="">Menu</h1>
+            <h1>Rewards</h1>
+            <h1>Gift Cards</h1>
+          </div>
+
+          <div className="border-t border-gray-300 shadow-lg shadow-black mx-5 my-5"></div>
+        </div>
+      </Drawer>
     </header>
   );
 };
